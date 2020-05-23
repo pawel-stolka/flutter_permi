45d3542e160a0e0244f819cb7854a8b8f6944aa0
pawel.stolka@gmail.com <pawel.stolka@gmail.com>
2020-05-23 13:05:35 +0200

    init

---
+++ android/app/src/main/java/com/robimy/permi/MainActivity.java
@@ -0,0 +1,6 @@
+package com.robimy.permi;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/enums/connectivity_status.dart
@@ -0,0 +1,5 @@
+enum ConnectivityStatus {
+  WiFi,
+  Mobile,
+  Offline
+}
